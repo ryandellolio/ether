7b346dc44d842024a1c2883564213983576d78b5
Ryan Dellolio <ryan@dellol.io>
2019-03-13 22:14:46 -0400

    establish 3 main POCs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var dns = require('dns');
 var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
+var aes256 = require('aes256');
 
 /*
 //create a server object:
@@ -11,26 +13,41 @@ http.createServer(function (req, res) {
 
 */
 
+//-----1-----create and read sqlite database
+
 var db = new sqlite3.Database('storage');
-var read = dns.resolveTxt('dns-field-test787.dellol.io', function (err, entries, family) {   //query dns and write response to entries variable
-  console.log(JSON.stringify(entries));
-});
-
-
 db.serialize(function() {
   db.run(`
-  
+
   CREATE TABLE IF NOT EXISTS "content" (
     "field_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
     "name"	TEXT,
     "value"	TEXT
   );
-  
+
   `);
 
   db.each("SELECT field_id AS id, name, value FROM content", function(err, row) {
-      console.log(row.id + " " + row.name + " "+ row.value + '\n');
+      console.log("The field id is " + row.id + " and the field name is " + row.name + " and the value is " + row.value + '\n');
   });
+
+
+});
+db.close();
+
+
+//-----1-----read the sqlite database file
+
+fs.readFile('storage', 'utf8', function(err, data) {  
+    if (err) throw err;
+    
+    console.log(data);
+    
 });
 
-db.close();
+//-----2-----read the sqlite database file
+
+
+var read = dns.resolveTxt('dns-field-test787.dellol.io', function (err, entries, family) {   
+  console.log(JSON.stringify(entries));
+});
