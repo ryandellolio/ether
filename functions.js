e888ef74e4aa3d92865cd968df64119bd4c7dd11
Ryan Dellolio <ryan@dellol.io>
2019-03-14 13:58:45 -0400

    working decryption from TXT to console

---
+++ functions.js
@@ -0,0 +1,79 @@
+var http = require('http');
+var dns = require('dns');
+var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
+var aes256 = require('aes256');
+var moment = require('moment');
+
+
+/*
+//create a server object:
+http.createServer(function (req, res) {
+  //code here
+
+}).listen(80); //the server object listens on port 80
+
+*/
+
+
+var db = new sqlite3.Database('storage.db');
+
+db.serialize(function() {
+
+
+  //--------- read sqlite database to console
+
+  db.each("SELECT field_id AS id, name, value FROM content", function(err, row) {
+      console.log("The field id is " + row.id + " and the field name is " + row.name + " and the value is " + row.value + '\n');
+  });
+  
+
+//----------save dump sqlite to raw sql file
+
+  var unix_time = moment().unix();
+
+  const { exec } = require('child_process');
+  exec('sqlite3 storage.db .dump > raw-streaming.sql', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+
+
+});
+
+db.close();
+
+
+//---------read a sqlite database file
+
+fs.readFile('storage.db', 'utf8', function(err, data) {  
+    if (err) throw err;
+    console.log(data);
+});
+
+
+//----------query dns
+
+var read = dns.resolveTxt('dns#v1-1.dellol.io', function (err, entries, family) {   
+  
+  var json_entries = JSON.stringify(entries);
+  console.log(json_entries);
+
+
+  //------------write file from dns query
+  
+        fs.writeFile('aes256-streaming.sql', json_entries, function(err) {  
+          if (err) throw err;
+          console.log("file written")
+         });
+
+
+});
+
+
+
+
--- index.js
+++ index.js
@@ -6,72 +6,20 @@ var aes256 = require('aes256');
 var moment = require('moment');
 
 
-/*
-//create a server object:
-http.createServer(function (req, res) {
-  //code here
+//given an AES encrypted DNS txt field, decrypt with given key and display in log
 
-}).listen(80); //the server object listens on port 80
-
-*/
-
-
-var db = new sqlite3.Database('storage.db');
-
-db.serialize(function() {
-
-
-  //--------- read sqlite database to console
-
-  db.each("SELECT field_id AS id, name, value FROM content", function(err, row) {
-      console.log("The field id is " + row.id + " and the field name is " + row.name + " and the value is " + row.value + '\n');
-  });
+var read = dns.resolveTxt('dns#v1-1.dellol.io', function (err, entries, family) {   
   
-
-//----------save dump sqlite to raw sql file
-
-  var unix_time = moment().unix();
-
-  const { exec } = require('child_process');
-  exec('sqlite3 storage.db \'.dump\' > raw-streaming.sql', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  });
-
-
-});
-
-db.close();
-
-
-//---------read a sqlite database file
-
-fs.readFile('storage.db', 'utf8', function(err, data) {  
-    if (err) throw err;
-    console.log(data);
-});
-
-
-//----------query dns
-
-var read = dns.resolveTxt('dns-field-test787.dellol.io', function (err, entries, family) {   
+  var encrypted = entries[0][0];
+  var key = 'key';
+  var decrypted = aes256.decrypt(key, encrypted);
   
-  var json_entries = JSON.stringify(entries);
-  console.log(json_entries);
-
-
-  //------------write file from dns query
+  console.log(decrypted);
   
-        fs.writeFile('file', json_entries, function(err) {  
-          if (err) throw err;
-          console.log("file written")
-         });
+  //use this to generate test encrypted strings
+  //console.log('encrypter: ' + aes256.encrypt(key, "my name is ryan"));
 
-
+  
 });
 
 
