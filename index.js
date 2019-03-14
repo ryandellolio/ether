b59fa5cf2e3de6d828730a31a222d12aed44baa5
Ryan Dellolio <ryan@dellol.io>
2019-03-13 20:05:51 -0400

    dns and http server hello world
    dns and http server hello world

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var http = require('http');
+var dns = require('dns');
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var w3 = dns.resolveTxt('dns-field-test787.dellol.io', function (err, responses, family) {
+  var entries = responses;
+});
+
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
