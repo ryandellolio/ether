afdf3d9a24123d1b978d6d7e1cf170a29b2ca68c
Ryan Dellolio <ryan@dellol.io>
2019-03-15 20:22:48 -0400

    stable point

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
 var dnsDB = require('./dnsDB.js');
 var sqlite3 = require('sqlite3').verbose();
 
-var container = new dnsDB('dns#v1-1.dellol.io', "key", function ( db ){  //creates a sqlite3 db
+dnsDB('dns#v1-1.dellol.io', "key", function ( db ){  //creates a sqlite3 db from a DNS call
 
   db.serialize(function() {         //use as you normally would per https://www.npmjs.com/package/sqlite3
-
     db.each("SELECT field_id as id, name, value FROM content WHERE id = '1'", function(err, row) {  
       
       if(err)
         console.log(err);
       if(row)
         console.log(row.id + " | " + row.name + " | " + row.value);
-      
       
     }); 
 
